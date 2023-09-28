baf80e45943a0a0ba909daedb88d8a5b42ea65c7
zhiying <j7114140038@gmail.com>
2023-09-28 15:42:21 +0800

    update home page

--- App.tsx
+++ App.tsx
@@ -1,118 +1,79 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import React from 'react';
-import type {PropsWithChildren} from 'react';
+import React, { useEffect, useState } from 'react';
+import { Provider } from 'react-redux';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
   Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+  HStack,
+  Switch,
+  useColorMode,
+  NativeBaseProvider,
+  extendTheme,
+} from 'native-base';
+import { config } from './src/constants/Config';
+import { store } from './src/state/redux';
+import { setStatusBarHidden, StatusBar } from 'expo-status-bar';
+import RootNavigator from './src/router';
+import * as NavigationBar from 'expo-navigation-bar';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { useIsForeground } from './src/hooks/useIsForeground';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+// Define the config
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+// extend the theme
+export const theme = extendTheme({ config });
 
-function Section({children, title}: SectionProps): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+export default function App() {
+  const appStateVisible = useIsForeground();
+  const [timer, setTimer] = useState<NodeJS.Timeout | null>(null);
+
+  useEffect(() => {
+    const sub = NavigationBar.addVisibilityListener(({ visibility }) => {
+      if (visibility === 'visible') {
+        if (timer !== null) {
+          clearTimeout(timer);
+        }
+        setTimer(
+          setTimeout(() => {
+            NavigationBar.setVisibilityAsync('hidden');
+          }, 2000)
+        );
+      }
+    });
+    setStatusBarHidden(true, 'slide');
+    NavigationBar.setBehaviorAsync('inset-swipe');
+    return () => {
+      sub.remove();
+      if (timer !== null) {
+        clearTimeout(timer);
+      }
+    };
+  }, [appStateVisible]);
+
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
+    <Provider store={store}>
+      <NativeBaseProvider>
+        <GestureHandlerRootView style={{ flex: 1 }}>
+          <RootNavigator />
+        </GestureHandlerRootView>
+        <StatusBar hidden={true} />
+      </NativeBaseProvider>
+    </Provider>
   );
 }
 
-function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
+// Color Switch Component
+function ToggleDarkMode() {
+  const { colorMode, toggleColorMode } = useColorMode();
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
+    <HStack space={2} alignItems="center">
+      <Text>Dark</Text>
+      <Switch
+        isChecked={colorMode === 'light' ? true : false}
+        onToggle={toggleColorMode}
+        aria-label={
+          colorMode === 'light' ? 'switch to dark mode' : 'switch to light mode'
+        }
       />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+      <Text>Light</Text>
+    </HStack>
   );
 }
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
-
-export default App;
